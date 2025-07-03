cf133129fefb0d6a74faf4848e032cd4fb19ed2f
emarimon <emmamarimontero.@gmail.com>
2025-07-03 03:13:49 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import Landing from './components/Landing.tsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Landing />
