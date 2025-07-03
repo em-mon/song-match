17c1e81b999a28f645e6ba0e839d420fc96a93e4
emarimon <emmamarimontero.@gmail.com>
2025-07-03 03:09:47 -0500

    Initial commit

---
+++ src/components/Landing.tsx
@@ -0,0 +1,30 @@
+import { motion } from "framer-motion";
+
+export default function Landing() {
+    return (
+    <div className="landing">
+        <div>
+            <motion.h1
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 3, delay: 0 }}>
+                WANT TO KNOW<br />WHAT YOUR PHOTO<br />SOUNDS LIKE?
+            </motion.h1>
+            <motion.h2
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 2, delay: 2 }}>
+                NOW YOU CAN WITH YOUR PICTURES, YOUR MOOD, AND YOUR MUSIC.
+            </motion.h2>
+        </div>
+        <div className="getStarted">
+            <motion.button
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 2, delay: 3.5 }}>
+                Get started!
+            </motion.button>
+        </div>
+    </div>
+    );
+}
